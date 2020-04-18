f700d762023a8f7a12a14d8585391bf59e5ae31e
Daniel Johannesson <daniel@johannesson.cz>
2020-04-18 23:19:55 +0200

    Updates

---
+++ precache-manifest.d9b9d62d0534f7cd4445851eca310623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a787423e59731adaa247fb9281d940ba",
+    "url": "/the-game/index.html"
+  },
+  {
+    "revision": "f706013bd19072ea0ba6",
+    "url": "/the-game/static/css/main.5910399f.chunk.css"
+  },
+  {
+    "revision": "1364444fa6fb714a7f3c",
+    "url": "/the-game/static/js/2.ac6de186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-game/static/js/2.ac6de186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f706013bd19072ea0ba6",
+    "url": "/the-game/static/js/main.2394d750.chunk.js"
+  },
+  {
+    "revision": "a0e0ab3975fda1c7c8fc",
+    "url": "/the-game/static/js/runtime-main.ba8038cf.js"
+  }
+]);
